71e696ecc9dfbf9922871761e745b2534a20ffb6
Gremlin4544 <alve@mckethen.com>
2020-02-13 18:42:46 -0500

    passed 2 tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,20 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders App without crashing", () => {
-  render(<App />);
+test("concise test - renders contact form header", () => {
+  const {getByText} = render(<App />);
+  getByText(/First Name/i);
 });
+
+// test("renders App without crashing", () => {
+//   //Arrange
+//   const { getByText } = render(<App />);
+
+//   //Act
+//   const header = getByText (/hello there/i);
+
+//   //Assert
+//   expect(header).toBeInTheDocument();
+//   expect(header).toBeTruthy();
+//   expect(header).not.toBeFalsy();
+// });
